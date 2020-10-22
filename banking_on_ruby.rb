e482c309bee08e995e9e975639ecc9647abcd276
lcnde <176673026+lcnde@users.noreply.github.com>
2020-10-22 15:39:57 +0200

    first commit

---
+++ banking_on_ruby.rb
@@ -0,0 +1,34 @@
+class Account
+    attr_reader :name, :balance
+      def initialize(name, balance=100)
+        @name = name
+        @balance = balance
+      end
+      
+      def display_balance(pin_number)
+        if pin == pin_number
+          puts "Balance: $#{@balance}."
+          else
+          puts pin_error
+        end
+      end
+    
+      def withdraw(pin_number, amount)
+        if pin_number == pin
+          @balance -= amount
+          puts "Withdrew #{amount}. New balance: $#{@balance}."
+          else
+          puts pin_error
+        end
+      end
+    
+      private
+      def pin
+        @pin = 1234
+      end
+      def pin_error
+        return "Access denied: incorrect PIN."
+      end
+    end
+    
+checking_account = Account.new("Marrymejuliet", "1_000_000_000")
